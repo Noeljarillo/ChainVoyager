19557a5a58917661aa0fe18697aeaf6c2baf6eb2
Javier R <javier.rios-urbano@epitech.eu>
2024-11-16 17:38:17 +0100

    fix: add `cors` handling

--- app/main.py
+++ app/main.py
@@ -8,11 +8,15 @@
 
 import os
 from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
 from agents import DeFiAgent
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/chat', methods=['POST'])
+@cross_origin()
 def chat_endpoint():
     try:
         data = request.get_json()
